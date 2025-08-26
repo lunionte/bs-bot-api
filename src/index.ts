af1f62f8507fcfbb37f651c6ee52c4312e7bbcd7
Italo <italoteles399@gmail.com>
2025-08-26 00:17:21 -0300

    adicionado suporte a CORS e middleware de cookie no servidor

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import express from "express";
 import { loginRoute } from "./routes/LoginRoute";
+import { userRoute } from "./routes/UserRoute";
 
 const app = express();
+app.use(cookieParser());
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            const allowedOrigins = ["http://localhost:3000", "https://f176564043f1.ngrok-free.app"];
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("CORS not allowed"));
+            }
+        },
+        credentials: true,
+    })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", loginRoute);
+app.use("/api/users", userRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
